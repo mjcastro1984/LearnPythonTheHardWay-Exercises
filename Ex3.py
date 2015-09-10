00c1010fa4dbe7a7e14c74fc4f86b38162642cf9
mjcastro1984 <miguel.castro@cdw.com>
2015-09-10 12:22:56 -0400

    Initial commit to GitHub

---
+++ Ex3.py
@@ -0,0 +1,27 @@
+print "I will now count my chickens:"
+
+# the following lines count the number of chickens
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+# this line counts the number of eggs
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# this line checks that the statement above is true or false
+print 3.0 + 2.0 < 5.0 - 7.0
+
+# these lines break down the math above
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2	
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
