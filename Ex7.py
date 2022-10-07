39726842df87ef310196c2cede027fa0dbe89e9e
Miguel Castro <mjcastro1984@gmail.com>
2022-10-07 14:20:12 -0400

    test an update to Ex7.py

--- Ex7.py
+++ Ex7.py
@@ -1,7 +1,7 @@
-print "Mary had a little lamb."
-print "It's fleece was white as %s." % 'snow'
-print "And everywhere that Mary went."
-print "." * 10 # what'd that do?
+print ("Mary had a little lamb.")
+print ("It's fleece was white as %s." % 'snow')
+print ("And everywhere that Mary went.")
+print ("." * 10 # what'd that do?)
 
 end1 = "C"
 end2 = "h"
